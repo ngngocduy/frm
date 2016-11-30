cccd5b351c787ead0dbef7c4a130bef2a95d6695
ngngocduy <duynn@bsdvn.com>
2016-11-30 20:56:01 +0700

    dsa
    dsa

--- Owner/UpdateTinhtrangthuhoach/UpdateTinhtrangthuhoach/UpdateTinhtrangthuhoach.cs
+++ Owner/UpdateTinhtrangthuhoach/UpdateTinhtrangthuhoach/UpdateTinhtrangthuhoach.cs
@@ -27,20 +27,23 @@ namespace UpdateTinhtrangthuhoach
             if (target.Contains("statuscode") && ((OptionSetValue)target["statuscode"]).Value == 100000000) // da duyet
             {
                 Entity lenhdon = service.Retrieve(target.LogicalName, target.Id,
-                    new ColumnSet(new string[] { "new_thuacanhtac", "new_lenhdoncuoi" }));
+                    new ColumnSet(new string[] { "new_thuacanhtac", "new_lenhdoncuoi","new_loaihopdong" }));
 
-                if (!lenhdon.Contains("new_thuacanhtac"))
-                    throw new Exception("Lệnh đốn không có chi tiết HĐ mía");
+                if (lenhdon.Contains("new_loaihopdong") && ((OptionSetValue)lenhdon["new_loaihopdong"]).Value == 100000000)
+                {
+                    if (!lenhdon.Contains("new_thuacanhtac"))
+                        throw new Exception("Lệnh đốn không có chi tiết HĐ mía");
 
-                Entity chitiethdmia = service.Retrieve("new_thuadatcanhtac", ((EntityReference)lenhdon["new_thuacanhtac"]).Id,
-                    new ColumnSet(new string[] { "new_tinhtrangthuhoach" }));
+                    Entity chitiethdmia = service.Retrieve("new_thuadatcanhtac", ((EntityReference)lenhdon["new_thuacanhtac"]).Id,
+                        new ColumnSet(new string[] { "new_tinhtrangthuhoach" }));
 
-                if (lenhdon.Contains("new_lenhdoncuoi") && (bool)lenhdon["new_lenhdoncuoi"] == true)
-                    chitiethdmia["new_tinhtrangthuhoach"] = new OptionSetValue(100000002);
-                else
-                    chitiethdmia["new_tinhtrangthuhoach"] = new OptionSetValue(100000001);
+                    if (lenhdon.Contains("new_lenhdoncuoi") && (bool)lenhdon["new_lenhdoncuoi"] == true)
+                        chitiethdmia["new_tinhtrangthuhoach"] = new OptionSetValue(100000002);
+                    else
+                        chitiethdmia["new_tinhtrangthuhoach"] = new OptionSetValue(100000001);
 
-                service.Update(chitiethdmia);
+                    service.Update(chitiethdmia);
+                }
             }
         }
     }
